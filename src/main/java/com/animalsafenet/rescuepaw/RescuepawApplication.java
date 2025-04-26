5c0e8fb6abacd509fc11396aa12a91d3d68be3b2
Kate Ne <jekanest@yahoo.com>
2025-04-26 20:27:06 +0300

    initial commit

---
+++ src/main/java/com/animalsafenet/rescuepaw/RescuepawApplication.java
@@ -0,0 +1,13 @@
+package com.animalsafenet.rescuepaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescuepawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescuepawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animalsafenet/rescuepaw/RescuepawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalsafenet.rescuepaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescuepawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
